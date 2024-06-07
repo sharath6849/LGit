f05291238f53638528af979eeb80e849b8403c6a
sharath <sharath6849@gmail.com>
2024-06-07 18:04:40 +0530

    sip added

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("addition"+Calculator.add(2,3));
         System.out.println("interset" + interestCalculator.calculate(10000000,8,5));
+        System.out.println("sip"+SIP.sipCal(5,5,5));
     }
 }
---
+++ SIP.java
@@ -0,0 +1,5 @@
+public class SIP {
+    public static int sipCal(int a,int t,int d){
+        return a*t*d;
+    }
+}
