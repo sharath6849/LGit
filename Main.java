6edd614702eb7f9445c34273d85a3ef4435ac0a8
sharath <sharath6849@gmail.com>
2024-06-07 18:13:15 +0530

    added comp int cal

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("addition"+Calculator.add(2,3));
-        System.out.println("interset" + interestCalculator.calculate(10000000,8,5));
+        System.out.println("compound interset" + CIcal.comintcal(9,9));
     }
 }
